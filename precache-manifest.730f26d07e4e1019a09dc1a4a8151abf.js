1a8872578964412ddf47dbbcbe24c3c74d250752
loki2k <jeremylagarto@gmail.com>
2020-12-13 10:37:05 -0800

    Updates

---
+++ precache-manifest.730f26d07e4e1019a09dc1a4a8151abf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b51942fe375c84c53750674fc5c80e9",
+    "url": "/color-generator/index.html"
+  },
+  {
+    "revision": "73e7281966adbf06afaf",
+    "url": "/color-generator/static/css/main.f6e8d1ab.chunk.css"
+  },
+  {
+    "revision": "cf6bca8e816ecf72b2d4",
+    "url": "/color-generator/static/js/2.11f691c1.chunk.js"
+  },
+  {
+    "revision": "1dbab337b90e8f2eb0a4a79d2378f49b",
+    "url": "/color-generator/static/js/2.11f691c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73e7281966adbf06afaf",
+    "url": "/color-generator/static/js/main.5a959d12.chunk.js"
+  },
+  {
+    "revision": "ed92ef5652ce8f57a79c",
+    "url": "/color-generator/static/js/runtime-main.c4c3dd28.js"
+  }
+]);
